f00ee53eceee3b93e2046e607c30938883dcbe6f
Peter Loos <peter.loos@gmx.de>
2023-11-13 13:18:15 +0100

    Stand Montag 13.30

---
+++ Aufgabe_01.md
@@ -0,0 +1,14 @@
+### Aufgabe 1:
+
+Berechnen Sie das Maximum dreier ganzer Zahlen.
+
+### Aufgabe 2:
+
+Entscheiden Sie, ob eine Jahrzahl für ein Schaltjahr steht oder nicht?
+
+Verwenden Sie dabei die Hilfestellung des folgenden Bilds:
+
+
+<img src="Schaltjahr.jpg" width="450">
+
+*Abbildung* 1: Bestimmung eines Schaltjahrs.
--- C_Beginners_November_2023/Program.c
+++ C_Beginners_November_2023/Program.c
@@ -3,28 +3,165 @@
 void erste_Schritte_01()
 {
     int n;  // hier wird eine Variable (n) vereinbart / definiert. 
+    int x;
 
-    n = 123;   // Wertzuweisung
+    n = 5;   // Wertzuweisung
+    x = n;   // hier wird n gelesen
+ 
+    n = 10;   // 2. Wertzuweisung
+              // hier wird n geschrieben
 
-    n = 456;   // 2. Wertzuweisung
+    n = 2 * n + 1;  // hier wird n zuerst gelesen ..und 
+                    // dann in DIESELBE Variable zurückgeschrieben
 
     // n = 21474836999999;    // zu gross, wird abgeschnitten
 
     printf("Zahl = %d\n", n );
 }
 
-
-
-
-
 void erste_Schritte_02()
+{
+    double x = 0.0; // Variable mit Namen x wird definiert
+
+    x = 55.55;
+
+    x = 1.0 / 3.0;
+
+    // Vorsichtig:
+
+    int n = 123;
+
+    x = n;   // das geht: Eine ganze Zahl passt in eine Kommazahl:  123.0
+
+   // n = x;  // VORSICHT: : '=': conversion from 'double' to 'int', 
+            // possible loss of data: Abschneiden
+
+
+    printf("Zahl = %f\n", x);  // %f: Ausgabe Gleitpunkt, 3 Nachkommastellen
+}
+
+
+void erste_Schritte_ueberlauf()
+{
+    int grosseZahl;
+
+    grosseZahl = 2147483647;   // 2 Milliarden ...
+
+    printf("Zahl = %d\n", grosseZahl);  
+
+    // Überlauf // Overflow // Unterlauf  // underflow
+
+    grosseZahl = grosseZahl + 1;          // erwartet: 214748364
+
+                                          // ABER: -2147483648 ist das Ergebnis
+
+    printf("Zahl = %d\n", grosseZahl);
+}
+
+void erste_Schritte_ueberlauf_02()
+{
+    double grosseZahl;
+
+    grosseZahl = 2147483647;   // 2 Milliarden ...
+
+    printf("Zahl = %f\n", grosseZahl);
+
+    printf("Zahl = %%\n");
+
+    // Überlauf // Overflow // Unterlauf  // underflow
+
+    grosseZahl = grosseZahl + 1;          // erwartet: 214748364
+
+    // ABER: -2147483648 ist das Ergebnis
+
+    printf("Zahl = %.30f\n", grosseZahl);
+}
+
+
+void erste_Schritte_03()
 {
   //  printf("%d\n", sizeof (long long));
 }
 
+
+
+
+void erste_Schritte_if_statement()
+{
+    int n = 0;  // Varianblen vereinbaren und vorbelegen
+    int m = 0;
+
+    n = 5;
+    m = 7;
+
+    if ( n > m )    // Vergleichsoperator 'grösser'
+    {
+        printf("n is greater than m\n");
+    }
+    else
+    {
+        printf("n is NOT greater than m\n");
+    }
+
+    printf("hier geht es weiter\n");
+}
+
+
+void erste_Schritte_if_statement_02 ()
+{
+    int n = 0;  // Variablen vereinbaren und vorbelegen
+    int m = 0;
+
+    int x = 11;
+    int y = 10;
+
+    n = 7;
+    m = 7;
+
+    // 2 Bedingungen
+
+    if ( n != m || x > y ) 
+    {
+        printf("Bedingung erfuellt\n");
+    }
+    else
+    {
+        printf("Bedingung nicht erfuellt\n");
+    }
+
+    printf("hier geht es weiter\n");
+}
+
+void erste_Schritte_if_statement_03()
+{
+    int n = 0;
+
+    n = 4;
+
+    if (n == 1)
+    {
+        printf("n hat den Wert 1\n");
+    }
+    else if ( n == 2 )
+    {
+        printf("n hat den Wert 2\n");
+    }
+    else if (n == 3)
+    {
+        printf("n hat den Wert 3\n");
+    }
+    else
+    {
+        printf("n hat weder den Wert 1 oder 2 oder 3\n");
+    }
+
+    printf("hier geht es weiter\n");
+}
+
+
 void main()
 {
-    erste_Schritte_01();
+    erste_Schritte_if_statement_03();
 }
 
 // Einsprungpunkt
